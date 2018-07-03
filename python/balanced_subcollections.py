516a99861527210820156cf2030908bda6b85f91
jassler <erifetim@gmail.com>
2018-07-03 23:46:03 +0200

    Balanced latex extension

--- python/balanced_subcollections.py
+++ python/balanced_subcollections.py
@@ -1,50 +1,55 @@
 from balanced_collections import find_balancing_weights
 from sympy import N
 from sympy.core.numbers import Float
-import itertools
-from multiprocessing.pool import Pool
-from multiprocessing import Lock
-from multiprocessing.managers import Array
+from itertools import chain, combinations
 import re
+import sys
+
+file = open('b.tex', 'w')
 
 def find_balanced_subcollections(collection, players):
     '''
     Find all minimal balanced collections of coalitions
     '''
+    def powerset_generator(collection):
+        for subset in chain.from_iterable(combinations(collection, r) for r in range(len(collection)+1)):
+            yield set(subset)
     # powerset algorithm from here: https://gist.github.com/michaelkrisper/5595918
-    powerset = [x for length in range(len(collection)+1) for x in itertools.combinations(collection, length)]
-    print('The powerset for {} players is {} long. '.format(len(players), len(powerset)))
+    # powerset = [x for length in range(len(collection)+1) for x in itertools.combinations(collection, length)]
 
-    possible_collections = []
-    solutions = []
+    powerset = powerset_generator(collection)
+    players_in_item = {}
+    for player in players:
+        players_in_item[player] = False
+    total_amount = 2 ** len(collection)
+    file.write('The powerset for {} players is {} items long.\n\n'.format(len(players), total_amount))
+    # solutions = []
+    possible_count = 0
+    count = 0
+
+    file.write('\\begin{longtable}{ | r l | c | }\n')
+    file.write('\\multicolumn{2}{c}{Collection of coalitions} & \multicolumn{1}{c}{Coefficients}\\\\[2pt]\n')
+
     # filter out collections that don't have all players participating
     for item in powerset:
         if len(item) > len(players):
             continue
-        players_in_item = []
+        for player in players_in_item.keys():
+            players_in_item[player] = False
         for coalition in item:
             for player in coalition:
-                if player not in players_in_item:
-                    players_in_item.append(player)
+                players_in_item[player] = True
         
-        if len(players) == len(players_in_item):
-            possible_collections.append(item)
+        if all(players_in_item.values()):
+            possible_count += 1
+            count = test_item(item, count)
+        
+    file.write('\\hline\n')
+    file.write('\\end{longtable}\\vspace{10pt}\n')
+    file.write('Program had to check {} possible solutions.\\vspace{{12pt}}\n\n'.format(possible_count))
     
-    def chunks(l, n):
-        """Yield successive n-sized chunks from l."""
-        for i in range(0, len(l), n):
-            yield l[i:i + n]
-    # pool = Pool(10)
-
-    # pool.map(test_item, possible_collections, chunksize=100)
-    
-    # pool.close()
-    # pool.join()
-    print('Program had to check {} possible solutions.\n'.format(len(possible_collections)))
-    for collection in possible_collections:
-        test_item(collection)
-    
-    return solutions
+    # return solutions
+    return count
 
 def is_minimal_balanced(result):
     # collection is minimal balanced, if result isn't conclusive / not an absolute value
@@ -58,73 +63,56 @@ def is_minimal_balanced(result):
             return False
     return True
 
-
-solutions = []
-
 # filter out not minimal balanced items
-def test_item(item):
+def test_item(item, count):
     result = find_balancing_weights(item, var_name='')
     if is_minimal_balanced(result):
+        if count % 10 == 0:
+            file.write('\\hline & & \\\\[-8pt]\n')
         collection = [str(x) for x in result.keys()]
-
-        #coefficients = [x for _,x in sorted(zip(collection, result.values()))]
-        #collection.sort()
         coefficients = result.values()
         # $\mathcal{B}_2 =$ & $\{\{1, 2\}, \{3\}\}$ & $(1, 1)$\\[2pt]
         coll_string = '{{{0}}}'.format(', '.join([str(x) for x in collection]))
         coll_string = coll_string.replace(r'{', r'\{').replace(r'}', r'\}')
         coeff_string = '\\left({0}\\right)'.format(', '.join([str(x) for x in coefficients]))
         coeff_string = re.sub(r'(\d*)/(\d*)', r'\\frac{\1}{\2}', coeff_string)
-
-        solutions.append('${0}$ & ${1}$'.format(coll_string, coeff_string))
-
-        # with lock:
-        #     count = counter.get()
-        #     counter.set(count + 1)
-        #     if count % 15 == 0:
-        #         print('\\hline & &\\\\[-8pt]')
-            
-        #     print('$\\mathcal{{B}}_{{{0}}} =$ & ${1}$ & ${2}$\\\\[2pt]'.format(count, coll_string, coeff_string))
-            
-    
+        count += 1
+        file.write('$\\mathcal{{B}}_{{{0}}} =$ & ${1}$ & ${2}$\\\\[2pt]\n'.format(count, coll_string, coeff_string))
+    return count
 
 if __name__ == '__main__':
-    print(r'\documentclass[10pt,a4paper,titlepage]{article}')
-    print(r'\usepackage[utf8]{inputenc}')
-    print(r'\usepackage{amsmath}')
-    print(r'\usepackage{amsfonts}')
-    print(r'\usepackage{amssymb}')
-    print(r'\usepackage{amsthm}')
-    print(r'\author{Felix Fritz}')
-    print(r'\title{A list of balanced collections of coalitions}')
-    print(r'\begin{document}')
-    print(r'\maketitle')
-    print(r'\tableofcontents')
-    print(r'\pagebreak')
-
-    for players in range (1, 6):
-        solutions.clear()
-        print('\\section{{Balanced collections of coalitions for $N = {0}$}}'.format(players))
+    
+    file.write('''\\documentclass[10pt,a4paper,titlepage]{article}
+\\usepackage[utf8]{inputenc}
+\\usepackage{amsmath}
+\\usepackage{longtable}
+\\usepackage{amsfonts}
+\\usepackage{amssymb}
+\\usepackage{amsthm}
+\\setlength{\\parindent}{0pt}
+\\author{Felix Fritz}
+\\title{A list of balanced collections of coalitions}
+\\begin{document}
+\\maketitle
+\\tableofcontents
+\\pagebreak
+''')
+    for players in range (1,8):
+        file.write('\\section{{Balanced collections of coalitions for $N = {0}$}}\n'.format(players))
 
         players = [i for i in range(1,players+1)]
-        powerset = [x for length in range(len(players)+1) for x in itertools.combinations(players, length)]
+        powerset = [x for length in range(len(players)+1) for x in combinations(players, length)]
         powerset.remove(())
-        find_balanced_subcollections(powerset, players)
-
-        print('For $N = \\{{{0}\\}}$, a total of {1} collections are balanced.\\vspace{{8pt}}\n'.format(', '.join([str(x) for x in players]), len(solutions)))
+        count = 0
         
-        print(r'\begin{tabular}{ | r l | c | }')
-        print(r'\multicolumn{2}{c}{Collection of coalitions} & \multicolumn{1}{c}{Coefficients}\\[2pt]')
-        print(r'\hline & & \\[-8pt]')
-        
-        for i, solution in enumerate(solutions):
-            print('$\\mathcal{{B}}_{{{0}}} =$ & {1}\\\\[2pt]'.format(i+1, solution))
+        count = find_balanced_subcollections(powerset, players)
 
-        print(r'\hline')
-        print(r'\end{tabular}')
-    print(r'\end{document}')
-    #print(solutions)
+        file.write('For $N = \\{{{0}\\}}$, a total of {1} collections are balanced.\\vspace{{8pt}}\n\n'.format(', '.join([str(x) for x in players]), count))
+        
+    file.write('\\end{document}')
+    file.close()
+    #file.write(solutions)
     # for solution in solutions:
     #     collection = [str(x) for x in solution.keys()]
     #     collection.sort()
-    #     print('{{{0}}}'.format(','.join(collection), end=''))
+    #     file.write('{{{0}}}'.format(','.join(collection), end=''))
